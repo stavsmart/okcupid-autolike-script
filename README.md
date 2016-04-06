68b427888e959716208ebbd69952297dd4e1d2a9
Pulkit Kathuria <kevincobain2000@gmail.com>
2016-04-07 01:37:06 +0900

    trust me, numbers game does't work.

---
+++ README.md
@@ -0,0 +1,9 @@
+####### Go Crazy
+
+OKC has no limits on their API
+
+####### How To
+
+- Navigate to http://www.okcupid.com/quickmatch
+
+- Either copy paste script as it is into your developer console or install this extension https://chrome.google.com/webstore/detail/custom-javascript-for-web/poakhlngfciodnhlhhgnaaelnpjljija and paste the code for okc.
---
+++ script.js
@@ -0,0 +1,21 @@
+setTimeout(function(){
+    $disabled = false;
+    if (!$disabled) {
+        var r = confirm("Like PPL on this Page");
+
+        if (r == true) {
+            var counter = 0;
+            setInterval(function(){
+                   txt = "You pressed OK!";
+                    elems = document.getElementsByClassName('cardactions-action cardactions-action--like')
+                    for(var i = 0; i< elems.length; i++) {
+                        if ( !elems[i].classList.contains("liked") ){
+                          counter++;
+                          elems[i].click();
+                          console.log('liked:' + counter)
+                        }
+                    }
+            }, 100);
+        }
+    }
+},1000);
